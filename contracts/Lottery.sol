31cd59361e8abd937024353401d3b1f35b10aa89
Luke Sheppard <sheppiegit@gmail.com>
2023-05-17 16:58:21 -0400

    Added the fulfillRandomWords and requestWinner to get a random number and take the random number mod lengthe of the players array to get the index of the player taht wins. We aqre emmiting some events like teh request for the random winner.

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -11,6 +11,7 @@ pragma solidity ^0.8.18;
 
 error Lottery__NotEnoughETHEntered();
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 contract Lottery is VRFConsumerBaseV2 {
     /**
@@ -18,17 +19,31 @@ contract Lottery is VRFConsumerBaseV2 {
      */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
 
     /**
      * Events
      */
     event LotteryEnter(address indexed player);
+    event RequestedLotteryWinner(uint256 indexed requestId);
 
     constructor(
         address vrfCoordinatorV2,
-        uint256 entranceFee
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
     ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     //get entrance fee
@@ -54,10 +69,21 @@ contract Lottery is VRFConsumerBaseV2 {
         //Request the random number
         //Once we get it,. do something with it
         //2 transaction process
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestedLotteryWinner(requestId);
     }
 
     function fulfillRandomWords(
         uint256 requestId,
         uint256[] memory randomWords
-    ) internal override {}
+    ) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+    }
 }
