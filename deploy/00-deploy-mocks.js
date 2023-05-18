29bac38891a8a0a405b5bb86b459fb1043095d84
Luke Sheppard <sheppiegit@gmail.com>
2023-05-18 11:59:01 -0400

    Added the deployment of the mock for VRFCoordinator.

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,12 +1,24 @@
 const { network } = require('hardhat');
 const { developmentChains } = require('../helper-hardhat-config');
 
+const BASE_FEE = ethers.utils.parseEther('0.25'); //0.25 is the premium. It costs 0.24 LINK
+const GAS_PRICE_LINK = 1e9; //calculated value based on the gas price of the chain
+
 module.exports = async ({ getNameAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNameAccounts();
-  const chainId = network.config.chainId;
 
   if (developmentChains.includes(network.name)) {
-    log('Local network detected....deploying!');
+    log('Local network detected....deploying mock!');
+    await deploy('VRFCoordinatorV2Mock', {
+      from: deployer,
+      log: true,
+      args: [BASE_FEE, GAS_PRICE_LINK],
+    });
+
+    log('Mocks deployed');
+    log('=================================================================');
   }
 };
+
+module.exports.tags = ['all', 'mocks'];
