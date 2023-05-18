a9f4ed19f53d308a6fe2777cb527077d9444893a
Luke Sheppard <sheppiegit@gmail.com>
2023-05-18 12:28:05 -0400

    Added the necessary arguments for the arguments of the lottery function, each different for the hardhat network and sepolia network.

--- deploy/01-deploy-lottery.js
+++ deploy/01-deploy-lottery.js
@@ -1,10 +1,44 @@
+const { network, ethers } = require('hardhat');
+const {
+  developmentChains,
+  networkConfig,
+} = require('../helper-hardhat-config');
+
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther('30');
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+  let vrfCoordinatorV2Address, subscriptionId;
+
+  if (developmentChains.includes(network.name)) {
+    const vrfCoordinatorV2Mock = await ethers.getContract(
+      'VRFCoordinatorV2Mock'
+    );
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+    const transactionResponse = await vrfCoordinatorV2Mock.createSubscription();
+    const transactionReceipt = await transactionResponse.wait(1);
+    subscriptionId = transactionReceipt.events[0].args.subId;
+    //Fund the subscription
+    //Usually need the link token on a real network
+    await vrfCoordinatorV2Address.fundSubcription(
+      subscriptionId,
+      VRF_SUB_FUND_AMOUNT
+    );
+  } else {
+    vrfCoordinatorV2Address = networkConfig[chainId]['vrfCoordinatorV2'];
+    subscriptionId = networkConfig[chainId]['subscriptionId'];
+  }
+
+  const entranceFee = networkConfig[chainId]['entranceFee'];
+  const gasLane = networkConfig[chainId]['gasLane'];
+  const callbackGasLimit = networkConfig[chainId]['callbackGasLimit'];
+  const interval = networkConfig[chainId]['interval'];
 
   const lottery = await deploy('Lottery', {
     from: deployer,
-    args: [],
+    args: [entranceFee, gasLane, subscriptionId, callbackGasLimit, interval],
     log: true,
   });
 };
