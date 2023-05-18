17a3f4c2d18bb08c94d81d051647bd872d0cfb9f
Luke Sheppard <sheppiegit@gmail.com>
2023-05-18 11:49:48 -0400

    updated hardhat config and packages with dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require('@nomicfoundation/hardhat-toolbox');
 require('@nomiclabs/hardhat-ethers');
+require('dotenv').config();
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
