91911a22d4dec73a71bf6e97d0a3511cd7c46228
Luke Sheppard <sheppiegit@gmail.com>
2023-05-18 11:36:47 -0400

    Updated hardhat config file and some deploy packages from hardhat.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,29 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('@nomiclabs/hardhat-ethers');
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+  },
+  solidity: '0.8.18',
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
 };
